41df02194696f528b5a94bb3502c0484e4a85ea4
Benn BIGGroup Al Islamicity <tamankuliner.islamicity@gmail.com>
2023-01-19 17:49:55 +0700

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Welcome to my website!")
+        fmt.Fprintf(w, "Welcome to our workshop website!")
     })
     http.ListenAndServe(":8080", nil)
 }
